d013c9a978f1af2c6cdbb1f420d816c2479c2ac1
Florence Li <florenceli@Florences-MBP.gateway>
2020-08-28 22:22:32 +1000

    hover text updates

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const bodyTag = document.querySelector("body")
 const progressTag = document.querySelector("div.progress")
+const closeTag = document.getElementById("close")
+const popupTag = document.getElementById("popup")
+const popupSmallTag = document.getElementById("popup-small")
 
 document.addEventListener("scroll", function () {
   const pixels = window.pageYOffset
@@ -8,4 +11,14 @@ document.addEventListener("scroll", function () {
 
   const percentage = pixels / totalScrollableDistance
   progressTag.style.width = `${100 * percentage}%`
+})
+
+closeTag.addEventListener("click", function () {
+  popupTag.style.display = "none";
+  popupSmallTag.style.display = "block";
+})
+
+popupSmallTag.addEventListener("click", function () {
+  popupTag.style.display = "block";
+  popupSmallTag.style.display = "none";
 })
